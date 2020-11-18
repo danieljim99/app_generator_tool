42f716104894bbcbada179182405fc00967b6c09
danieljim99 <djimenezf@alumnos.nebrija.es>
2020-11-18 14:55:02 +0100

    generate schemas

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# app_generator_tool
+# APP GENERATOR TOOL
+
+**USAGE**
+
+    - Define the types that are going to be used by the app in a yaml file.
+    - In the *.env* file add the yaml file path as it is in .env.sample file.
+    - Run *deno run --allow-read --allow-write index.ts* in the root folder.
+
+**YAML FILE FORMAT**
+
+    - All types must be inside the *types* array.
+    - The attributes types must be existing types in *TypeScript* or types defined in the yaml file.
+    - The first attribute must be an unique attribute of that type.
+    - In short, follow the *sample.yaml* format.
---
+++ src/app.ts
@@ -0,0 +1,79 @@
+import { YamlLoader } from "https://deno.land/x/yaml_loader/mod.ts";
+import { config } from "https://deno.land/x/dotenv/mod.ts";
+
+const readYaml = async (path: string) => {
+    const yamlLoader = new YamlLoader();
+    return await yamlLoader.parseFile(path) as { types: any[] };
+};
+
+const firstCapital = (word: string) => {
+    let result: string = "";
+    if ("[(".includes(word[0])) {
+        result = `${word[0]}${word[1].toUpperCase()}${word.slice(2)}`;
+    } else {
+        result = `${word[0].toUpperCase()}${word.slice(1)}`;
+    }
+    return result;
+};
+
+const createQuery = (type: Object) => {
+    const value = Object.values(type)[0];
+    const key = Object.keys(type)[0];
+    let result: string = "";
+    result = result.concat("type Query {");
+    result = result.concat(` getAll${key}: [${key}!]`);
+    result = result.concat(` get${key}(${Object.keys(value)[0]}: ${firstCapital(Object.values(value)[0] as string)}!): ${key}!`);
+    result = result.concat(" }");
+    return result;
+};
+
+const createMutation = (type: Object) => {
+    const value = Object.values(type)[0];
+    const key = Object.keys(type)[0];
+    let result: string = "";
+    result = result.concat("type Mutation {");
+    result = result.concat(` create${key}(`);
+    Object.keys(value).forEach((key, index) => {
+        result = result.concat(`${key}: ${firstCapital(Object.values(value)[index] as string)}! `);
+    });
+    result = result.concat(`): ${key}!`);
+    result = result.concat(` update${key}(`);
+    Object.keys(value).forEach((key, index) => {
+        result = result.concat(`${key}: ${firstCapital(Object.values(value)[index] as string)}! `);
+    });
+    result = result.concat(`): ${key}!`);
+    result = result.concat(` delete${key}(${Object.keys(value)[0]}: ${firstCapital(Object.values(value)[0] as string)}!): ${key}!`);
+    result = result.concat(" }");
+    return result;
+};
+
+const createType = (type: Object) => {
+    const value = Object.values(type)[0];
+    let result: string = "";
+    result = result.concat(`type ${Object.keys(type)[0]} {`);
+    Object.keys(value).forEach((key, index) => {
+        result = result.concat(` ${key}: ${firstCapital(Object.values(value)[index] as string)}!`);
+    })
+    result = result.concat(` }`);
+    return result;
+}
+
+const createSchema = (type: Object) => {
+    return `${createQuery(type)}\r\n${createMutation(type)}\r\n${createType(type)}\r\n`;
+};
+
+export const app = async () => {
+    const types = (await readYaml(config().YAML_FILE)).types;
+
+    if (!types || types.length === 0) {
+        throw new Error("No types specified");
+    }
+
+    const schemas = types.map(type => createSchema(type));
+
+    const encoder = new TextEncoder();
+
+    schemas.forEach((schema, index) => {
+        Deno.writeFile(`./outputs/${index}.graphql`, encoder.encode(schema));
+    });
+};
