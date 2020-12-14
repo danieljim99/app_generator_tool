e78e68e96f593a367696d6e2c2aebe919fd058e6
danieljim99 <djimenezf@alumnos.nebrija.es>
2020-12-14 09:52:56 +0100

    updated readme, updated generate types

--- README.md
+++ README.md
@@ -3,10 +3,10 @@
 ## USAGE
 * Define the types that are going to be used by the app in a yaml file.
 * In the *.env* file add the yaml file path as it is in .env.sample file.
-* Run *deno run --allow-read --allow-write --unstable index.ts* in the root folder.
+* Run *deno run --allow-read --allow-net index.ts* in the root folder.
 
 ## YAML FILE FORMAT
 * All types must be inside the *types* array.
-* The attributes types must be existing types in *TypeScript* or types defined in the yaml file.
+* The attributes types must be existing types in *TypeScript* or types defined before in the yaml file.
 * The first attribute must be an unique attribute of that type.
 * In short, follow the *sample.yaml* format.
--- src/app.ts
+++ src/app.ts
@@ -1,66 +1,127 @@
+import {
+    graphql,
+    GraphQLBoolean,
+    GraphQLInt,
+    GraphQLList,
+    GraphQLObjectType,
+    GraphQLSchema,
+    GraphQLString,
+} from "https://cdn.pika.dev/graphql/15.3.0";
+import { Application, Router } from "https://deno.land/x/oak@v6.2.0/mod.ts";
 import { YamlLoader } from "https://deno.land/x/yaml_loader/mod.ts";
 import { config } from "https://deno.land/x/dotenv/mod.ts";
-import { ensureDirSync } from "https://deno.land/std/fs/mod.ts";
 
 const readYaml = async (path: string) => {
     const yamlLoader = new YamlLoader();
     return await yamlLoader.parseFile(path) as { types: any[] };
 };
 
-const firstCapital = (word: string) => {
-    let result: string = "";
-    if ("[(".includes(word[0])) {
-        result = `${word[0]}${word[1].toUpperCase()}${word.slice(2)}`;
-    } else {
-        result = `${word[0].toUpperCase()}${word.slice(1)}`;
-    }
+const getGraphQLType = (type: string, createdTypes: GraphQLObjectType[]) => {
+    let result;
+
+    //TO-DO: Check if the type is Array
+
+    switch (type) {
+        default:
+            result = createdTypes.find(createdType => createdType.name === type);
+            if (!result) {
+                throw new Error(`The type ${type} does not exist`);
+            }
+            break;
+        case "string":
+            result = GraphQLString;
+            break;
+        case "number":
+            result = GraphQLInt;
+            break;
+        case "boolean":
+            result = GraphQLBoolean;
+            break;
+    };
+
     return result;
 };
 
-const createQuery = (type: Object) => {
-    const value = Object.values(type)[0];
-    const key = Object.keys(type)[0];
-    let result: string = "";
-    result = result.concat("type Query {");
-    result = result.concat(` getAll${key}: [${key}!]`);
-    result = result.concat(` get${key}(${Object.keys(value)[0]}: ${firstCapital(Object.values(value)[0] as string)}!): ${key}!`);
-    result = result.concat(" }");
-    return result;
-};
+const generateFields = (type: Object, createdTypes: GraphQLObjectType[]) => {
+    let result: Object = {};
 
-const createMutation = (type: Object) => {
-    const value = Object.values(type)[0];
-    const key = Object.keys(type)[0];
-    let result: string = "";
-    result = result.concat("type Mutation {");
-    result = result.concat(` create${key}(`);
-    Object.keys(value).forEach((key, index) => {
-        result = result.concat(`${key}: ${firstCapital(Object.values(value)[index] as string)}! `);
+    Object.keys(Object.values(type)[0]).forEach((key, index) => {
+        Object.defineProperty(result, key, {
+            writable: true,
+            enumerable: true,
+            configurable: true,
+            value: {
+                type: getGraphQLType(
+                    Object.values(Object.values(type)[0])[index] as unknown as string,
+                    createdTypes
+                )
+            },
+        });
     });
-    result = result.concat(`): ${key}!`);
-    result = result.concat(` update${key}(`);
-    Object.keys(value).forEach((key, index) => {
-        result = result.concat(`${key}: ${firstCapital(Object.values(value)[index] as string)}! `);
-    });
-    result = result.concat(`): ${key}!`);
-    result = result.concat(` delete${key}(${Object.keys(value)[0]}: ${firstCapital(Object.values(value)[0] as string)}!): ${key}!`);
-    result = result.concat(" }");
+
     return result;
 };
 
-const createType = (type: Object) => {
-    const value = Object.values(type)[0];
-    let result: string = "";
-    result = result.concat(`type ${Object.keys(type)[0]} {`);
-    Object.keys(value).forEach((key, index) => {
-        result = result.concat(` ${key}: ${firstCapital(Object.values(value)[index] as string)}!`);
-    })
-    result = result.concat(` }`);
-    return result;
-}
+const createObjectTypes = async (types: Object[]) => {
+    let result: GraphQLObjectType[] = [];
 
-const createSchema = (type: Object) => {
-    return `${createQuery(type)}\r\n${createMutation(type)}\r\n${createType(type)}\r\n`;
+    types.forEach(type => {
+        result.push(new GraphQLObjectType({
+            name: Object.keys(type)[0],
+            fields: generateFields(type, result),
+        }));
+    });
+
+    return result;
+};
+
+const createSchema = async (types: Object[]) => {
+    const objectTypes = await createObjectTypes(types);
+    const queryObjectType = new GraphQLObjectType({
+        name: "Query",
+        fields: {
+            getUsers: {
+                type: new GraphQLList(objectTypes[0]),
+            },
+            getActivities: {
+                type: new GraphQLList(objectTypes[1]),
+            }
+        },
+    });
+    const schema = new GraphQLSchema({
+        query: queryObjectType,
+    });
+    const resolver = {
+        //TO-DO: Generate resolvers automatically
+        getUsers: () => [
+            { name: "David", email: "david@test.com" },
+            { name: "Peter", email: "peter@test.com" },
+        ],
+        getActivities: () => [
+            { name: "Activity1", author: { name: "David", email: "david@test.com" } },
+            { name: "Activity2", author: { name: "Peter", email: "peter@test.com" } },
+        ],
+    };
+    const executeSchema = async (query: any) => {
+        const result = await graphql(schema, query, resolver);
+        return result;
+    };
+    const router = new Router();
+    router.post("/graphql", async ({ request, response }) => {
+        if (request.hasBody) {
+          const body = request.body({ type: "json" });
+          const bodyValue = await body.value;
+          const result = await executeSchema(bodyValue.query);
+          response.body = result;
+        } else {
+          response.body = "Query Unknown";
+        }
+    });
+    const app = new Application();
+    app.use(router.routes());
+    app.use(router.allowedMethods());
+    console.log("Server running on port 5000");
+    app.listen({ port: 5000 });
 };
 
 export const app = async () => {
@@ -72,11 +133,5 @@ export const app = async () => {
         throw new Error("No types specified");
     }
 
-    const schemas = types.map(type => createSchema(type));
-
-    await ensureDirSync("./outputs");
-
-    schemas.forEach((schema, index) => {
-        Deno.writeFile(`./outputs/${index}.graphql`, encoder.encode(schema));
-    });
+    createSchema(types);
 };
