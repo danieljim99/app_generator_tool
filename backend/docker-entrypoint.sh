8bd18cade7979060e4594e18d10a66690668ed67
daniel jimenez <daniel.jimenez@bq.com>
2021-04-24 13:26:37 +0200

    backend rename and tsconfig.json

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -xe
 if [ -d /root/config/ ]; then
-    cp /root/config/env /usr/src/app_generator/.env
+    cp /root/config/env /usr/src/backend/.env
 fi
 
 deno run --allow-read --allow-net index.ts
