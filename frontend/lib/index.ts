84e612c0383ac531ed78787f2be67f9d2a375fa0
danieljim99 <danieljim99@gmail.com>
2021-09-17 13:13:26 +0200

    removed unused import

--- frontend/lib/index.ts
+++ frontend/lib/index.ts
@@ -1,11 +1,7 @@
 import getEnv from '~/lib/getEnv.ts';
 import getTypes from '~/lib/getTypes.ts';
-import useMutation from '~/lib/useMutation.ts';
-import useQuery from '~/lib/useQuery.ts';
 
 export {
   getEnv,
-  getTypes,
-  useMutation,
-  useQuery
+  getTypes
 };
